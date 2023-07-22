c5a8ef0d5bce0acb6612e4eb7129520e7833f0ed
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-07-22 23:25:39 +0200

    [UPDATED] Web links

--- src/constants/index.js
+++ src/constants/index.js
@@ -139,16 +139,16 @@ const testimonials = [
             "I've never met a web developer who truly cares about their clients' success like Rick does.",
         name: "Chris Brown",
         designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
+        company: "Singidunum University",
+        image: singi,
     },
     {
         testimonial:
             "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
         name: "Lisa Wang",
         designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
+        company: "Singidunum University",
+        image: singi,
     },
 ];
 
@@ -172,7 +172,7 @@ const projects = [
             },
         ],
         image: carrent,
-        source_code_link: "https://github.com/korisnik-samson/travel-static",
+        source_code_link: "https://github.com/korisnik-samson/car-octo-system",
         weblink: "https://car-octo-system.vercel.app/"
     },
     {
